047edbf6ed3a3ab08cf2d982337ed20e66ca28fe
scafer <sandroacferreira@gmail.com>
2025-02-12 22:14:05 +0000

    chore: to content-base v0.4.4

---
+++ src/test/java/io/github/pricescrawler/util/MongoContainerTest.java
@@ -0,0 +1,28 @@
+package io.github.pricescrawler.util;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+@SpringBootTest
+@Testcontainers
+public abstract class MongoContainerTest {
+    protected static final MongoDBContainer mongoDBContainer;
+
+    static {
+        mongoDBContainer = new MongoDBContainer("mongo:7.0");
+        mongoDBContainer.start();
+    }
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    @DynamicPropertySource
+    static void setProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+}
